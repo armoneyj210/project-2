6b0db0006ec8f20b4c23090673183f1954e86b73
Joshua Armoney <armoneyj210@gmail.com>
2019-12-18 13:46:53 -0500

    creator model

---
+++ models/creator.js
@@ -0,0 +1,38 @@
+const mongoose = require("./connection");
+const Schema = mongoose.Schema;
+
+const CreatorSchema = new Schema({
+  name: String,
+  age: String,
+  characters: String,
+  pic: String
+});
+const creatorCollection = mongoose.model("Creator", CreatorSchema);
+
+const getCreators = () => {
+  return creatorCollection.find({});
+};
+
+const getCreatorbyId = id => {
+  return creatorCollection.findById(id);
+};
+
+const createCreator = creatorObject => {
+  return creatorCollection.create(creatorObject);
+};
+
+const deleteCreator = id => {
+  return creatorCollection.deleteOne({ _id: id });
+};
+
+const updateCreator = (id, updatedCreatorObject) => {
+  return creatorCollection.updateOne({ _id: id }, updatedCreatorObject);
+};
+
+module.exports = {
+  getCreators,
+  getCreatorbyId,
+  createCreator,
+  deleteCreator,
+  updateCreator
+};
